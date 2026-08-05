f108254090374e4a95fb0af610ab5090e3feb023
Anisia <anisia-baik@list.ru>
2026-08-05 13:28:32 +0200

    authorization complete.

---
+++ src/app/auth/auth.interface.ts
@@ -0,0 +1,4 @@
+export interface Tokenresponce {
+    access_token: string,
+    refresh_token: string
+}
--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,11 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
 export class LoginPageComponent {
+
+  private authService = inject(AuthService);
+  private router = inject(Router);
+  errorMessage = signal<string | null>(null);
+  isLoading = signal(false);
+
+  form = new FormGroup({
+    username: new FormControl('', { nonNullable: true, validators: Validators.required }),
+    password: new FormControl('', { nonNullable: true, validators: Validators.required })
+  })
+
+  onSubmit(): void {
+
+    if(this.form.invalid || this.isLoading() ){
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    this.isLoading.set(true);
+    this.errorMessage.set(null);
+
+    this.authService.login(this.form.getRawValue()).subscribe({
+      next: (token) => { 
+        this.isLoading.set(true);
+        console.log("Успешный вход", token); 
+        this.router.navigate(['/']);
+      },
+      error: (err) => { 
+        console.error(err);
+        this.isLoading.set(false);
+        this.errorMessage.set(
+          err.status === 401
+          ? 'Неверный логин или пароль'
+          : 'Что-то пошло не так - попробуйте пожалуйста позже'
+        ); 
+      }
+    })
+
+  }
 
 }
