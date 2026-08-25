ac4b9ca3083122123930d70ce6e23b1cccdb32af
Anisia Belkovskaia <anisia-baik@list.ru>
2026-08-25 18:05:25 +0200

    saving profile form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { SearchComponent } from './pages/search/search.component';
 import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
 import { LayoutComponent } from './common-ui/layout/layout.component';
 import { canActivateAuth } from './auth/access.guard';
+import { SettingsPageComponent } from './pages/settings-page/settings-page.component';
 
 export const routes: Routes = [
     { path: '', component: LayoutComponent, children: [
         { path: '', component: SearchComponent },
-        { path: 'profile/:id', component: ProfilePageComponent }
+        { path: 'profile/:id', component: ProfilePageComponent },
+        { path: 'settings', component: SettingsPageComponent }
     ],
     canActivate: [canActivateAuth]
     },
